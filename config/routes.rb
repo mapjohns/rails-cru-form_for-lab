8ddb83f1e115c50bcc1b3e0f8e5adf6f44971d00
Matthew <mattj20@gmail.com>
2022-07-15 06:54:57 -0700

    commit tree c441a45e3de0e0827667f964cd8e9bfea8ff1b4c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:index, :new, :create, :edit, :update, :show]
 end
